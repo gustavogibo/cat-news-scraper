af4f4aed036d732108e42b13835a75fd879a8e47
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-06-20 17:57:42 -0700

    listing the articles in the index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,43 +1,42 @@
 var express = require("express");
-
-var router = express.Router();
-
 var router = express.Router();
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
-
-// Initialize Express
-var app = express();
 
 // Require all models
 var db = require("../models/");
 
 router.get("/", function (req, res) {
 
-  res.render("index");
+  db.Article.find().then(function (articles) {
+
+    res.render("index", {articles: articles});
+
+  })
 
 })
 
 // A GET route for scraping the echoJS website
 router.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.lifewithcats.tv/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article.format-standard").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.link = $(element).find(".alignnone").attr("href");
+
+      if (result.link == undefined) {
+
+        result.link = $(element).find(".aligncenter").attr("href");
+
+      }
+      result.title = $(element).find(".entry-title").find("a").text();
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
