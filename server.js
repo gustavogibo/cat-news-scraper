8be07bc0626ce063a36b3e09a1f9d6bb87c5c43c
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-06-21 15:50:57 -0700

    making mongolab to work

--- server.js
+++ server.js
@@ -18,8 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/catscrap");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 // var db = require("./models");
 
